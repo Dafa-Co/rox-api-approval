36d3d047c6b0c76f80600763a31d603968b30e23
hossamghoneim <60117042+hossamghoneim@users.noreply.github.com>
2024-08-15 14:29:01 +0300

    Updates ( Validations && Reviews )

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,12 @@ import express, { NextFunction, Request, Response } from 'express';
 import { DriversEnum } from './Enums/DriversEnum';
 import { DriversFactory } from './Classes/DriversFactory';
 import { catchAsync } from './utils/catchAsync'; // Import the catchAsync utility
-import { query, validationResult } from 'express-validator';
+import { body, query, validationResult } from 'express-validator';
 
 const app = express();
 app.use(express.json());
 const port = 3000;
-const driversFactory = new DriversFactory(DriversEnum.googleDrive);
+const driversFactory = new DriversFactory(DriversEnum.oneDrive);
 
 app.get('/auth-redirect', catchAsync(async (req: Request, res: Response) => {
   const code = req.query.code as string;
@@ -26,12 +26,12 @@ app.get('/login', async (req, res) => {
   await driversFactory.login(req, res);
 });
 
-app.get('/get-key', query('vault_name').notEmpty(), query('wallet_id').notEmpty(), catchAsync(async (req: Request, res: Response) => {
+app.get('/get-key', query('vault_name').notEmpty().isAlpha(), body('wallet_id').notEmpty().isNumeric(), catchAsync(async (req: Request, res: Response) => {
   const result = validationResult(req);
   
-  if (result['errors']) {
+  if (result['errors'] && result['errors'].length > 0) {
     const errors = result['errors'];
-    const entries = errors.map((error: { path: string; msg: string; }) => [error.path, error.msg] as [string, string]); console.log('entries', entries);
+    const entries = errors.map((error: { path: string; msg: string; }) => [error.path, error.msg] as [string, string]);
     
     return res.status(422).send(Object.fromEntries(entries));
   }
@@ -39,25 +39,24 @@ app.get('/get-key', query('vault_name').notEmpty(), query('wallet_id').notEmpty(
   const folderName = req.query.vault_name as string;
   const fileName = req.query.wallet_id as string;
 
-  if (!folderName || !fileName) {
-    return res.status(400).send('Missing required fields');
-  }
-
   const content = await driversFactory.getKey(folderName, fileName);
   res.json({ content: { private_key: content } });
 }));
 
-app.post('/set-key', catchAsync(async (req: Request, res: Response) => {
+app.post('/set-key', body('vault_name').notEmpty().isAlpha(), body('wallet_id').notEmpty().isNumeric(), body('key').notEmpty().isString(), catchAsync(async (req: Request, res: Response) => {
+  const result = validationResult(req);
+  
+  if (result['errors'] && result['errors'].length > 0) {
+    const errors = result['errors'];
+    const entries = errors.map((error: { path: string; msg: string; }) => [error.path, error.msg] as [string, string]);
+    
+    return res.status(422).send(Object.fromEntries(entries));
+  }
+
   const folderName = req.body.vault_name as string;
   const fileName = req.body.wallet_id as string;
   const content = req.body.key as string;
-
-  if (!folderName || !fileName || !content) {
-    return res.status(400).send('Missing required fields');
-  }
-
   const response = await driversFactory.setKey(folderName, fileName, content);
-  console.log('response', response);
 
   res.status(200).send(`Key uploaded successfully`);
 }));
