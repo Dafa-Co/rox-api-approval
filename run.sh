ad913ae219ea136e52a649c6bc60ab1a9128deac
waer1 <elwaeryousef@gmail.com>
2024-08-27 14:16:31 +0300

    add the running script

--- run.sh
+++ run.sh
@@ -43,6 +43,7 @@ check_docker_installed || {
 # Function to display options and get user choice
 display_options() {
     while true; do
+        echo "" > .env
         echo "Please choose an option:"
         echo "1) AWS S3" # .env
         echo "2) One Drive" # credentials.json
@@ -58,7 +59,6 @@ display_options() {
                 read -p "Enter Bucket Secret: " bucket_secret
                 read -p "Enter Bucket Region: " bucket_region
                 read -p "Enter Bucket Name: " bucket_name
-                read -p "Enter AWS Directory: " aws_directory
 
                 # Write to .env file
                 echo "AWS_ENDPOINT=$aws_endpoint" >> .env
@@ -66,7 +66,7 @@ display_options() {
                 echo "BUCKETSECRET=$bucket_secret" >> .env
                 echo "BUCKETREGION=$bucket_region" >> .env
                 echo "BUCKETNAME=$bucket_name" >> .env
-                echo "AWS_DIRECTORY=$aws_directory" >> .env
+                echo "HANDLER=amazonS3" >> .env
 
                 file_to_mount=".env"
 
@@ -80,6 +80,7 @@ display_options() {
                     if [[ -f "$one_drive_path" ]]; then
                         cp "$one_drive_path" "$(dirname "$0")/credentials.json"
                         echo "Credentials have been copied to the script's directory."
+                        echo "HANDLER=oneDrive" >> .env
                         break
                     else
                         echo "File not found. Please enter a valid path."
@@ -97,6 +98,7 @@ display_options() {
                     if [[ -f "$google_drive_path" ]]; then
                         cp "$google_drive_path" "$(dirname "$0")/credentials.json"
                         echo "Credentials have been copied to the script's directory."
+                        echo "HANDLER=googleDrive" >> .env
                         break
                     else
                         echo "File not found. Please enter a valid path."
@@ -118,6 +120,7 @@ display_options() {
                 echo "DROPBOX_ACCESS_TOKEN=$dropbox_access_token" >> .env
                 echo "DROPBOX_CLIENT_ID=$dropbox_client_id" >> .env
                 echo "DROPBOX_CLIENT_SECRET=$dropbox_client_secret" >> .env
+                echo "HANDLER=dropbox" >> .env
 
                 file_to_mount=".env"
 
@@ -134,6 +137,22 @@ display_options() {
 # Get user choice
 display_options
 
+
+# ask the user for the domain or the ip of the vm
+read -p "Please enter the domain or the ip of the vm: " user_domain
+
+echo "Domain or IP: $user_domain"
+
+# Validate the domain or the IP
+if ! [[ "$user_domain" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
+    # This regex only checks format, not validity of the IP address
+    echo "Invalid IP address. Please enter a valid IP address."
+    exit 1
+fi
+
+echo "DOMAIN=$user_domain" >> .env
+echo "PORT=3000" >> .env
+
 # Ask for the port number
 read -p "Please enter the port you would like to run the application on: " user_port
 
@@ -144,6 +163,6 @@ if [[ ! "$user_port" =~ ^[0-9]+$ ]] || [ "$user_port" -lt 1 ] || [ "$user_port" 
 fi
 
 # Run the Docker container with the file mounted
-echo "Running Docker container on port $user_port..."
-docker run -d -p $user_port:$user_port -v "$(pwd)/$file_to_mount":/app/$file_to_mount rox-api-approval
+echo "Running the application on $user_domain:$user_port..."
+docker run -d -p $user_port:3000 -v "$(pwd)/$file_to_mount":/usr/src/app/$file_to_mount rox-api-approval
 echo "Container is running on port $user_port and $file_to_mount has been added inside the container."
