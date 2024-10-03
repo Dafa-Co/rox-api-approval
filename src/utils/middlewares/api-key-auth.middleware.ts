118b14184692f08889044c9871bd7314a3a53263
waer1 <elwaeryousef@gmail.com>
2024-10-03 13:28:41 +0300

    update the header key

--- src/utils/middlewares/api-key-auth.middleware.ts
+++ src/utils/middlewares/api-key-auth.middleware.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { API_KEY } from "../../server";
 
 export function checkApiKey(req: Request, res: Response, next: NextFunction) {
-  const apiKey = req.headers["x-api-key"];
+  const apiKey = req.headers["x-verify-key"];
   if (!apiKey || apiKey !== API_KEY) {
     return res.status(401).json({ error: "Unauthorized: Invalid API key" });
   }
